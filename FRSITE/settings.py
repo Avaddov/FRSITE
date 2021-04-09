d467fef67ee00b552806412ac807c7a2cd260435
FiendRiver <fiendriver@gmail.com>
2021-04-09 14:02:30 +0300

    herokufix

--- FRSITE/settings.py
+++ FRSITE/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1:8000', 'safe-everglades-39397.herokuapp.com']
 
 # Application definition
 
